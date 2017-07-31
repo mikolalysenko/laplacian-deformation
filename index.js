cc9d528c79437dd3c05845b190ebc4874fac5ba6
mikolalysenko <mikolalysenko@gmail.com>
2017-07-30 18:02:54 -1000

    shit's fucked

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function transpose (positions) {
   var x = new Float64Array(positions.length)
   var y = new Float64Array(positions.length)
   var z = new Float64Array(positions.length)
-  for (var i = 0; i < positions.lengths; ++i) {
-    const p = positions[i]
+  for (var i = 0; i < positions.length; ++i) {
+    var p = positions[i]
     x[i] = p[0]
     y[i] = p[1]
     z[i] = p[2]
@@ -19,30 +19,32 @@ function transpose (positions) {
 
 module.exports = function (cells, positions, handleIds) {
   var N = positions.length
+  var M = N + handleIds.length
   var coeffs = calcLaplacian(cells, positions)
   var lapMat = CSRMatrix.fromList(coeffs, N, N)
 
   // calculate position derivatives
   var tpositions = transpose(positions)
   var delta = [
-    lapMat.apply(tpositions[0]),
-    lapMat.apply(tpositions[1]),
-    lapMat.apply(tpositions[2])
+    lapMat.apply(tpositions[0], new Float64Array(N)),
+    lapMat.apply(tpositions[1], new Float64Array(N)),
+    lapMat.apply(tpositions[2], new Float64Array(N))
   ]
 
   // augment matrix
   for (var i = 0; i < handleIds.length; ++i) {
-    coeffs.push([i + N, handleIds[i], 1])
+    coeffs.push([handleIds[i], i + N, 1])
   }
-  var augMat = CSRMatrix.fromList(coeffs, N + handleIds.length, N)
+  var augMat = CSRMatrix.fromList(coeffs, N, M)
 
   // calculate square matrix
-  var mmt = csrgemtm(augMat, augMat.transpose())
+  var mmt = csrgemtm(augMat, augMat)
 
-  var pi = cmprecond(mmt)
-  var solve = ldl(augMat, N + handleIds.length, pi)
+  var pi = cmprecond(mmt, N)
+  var solve = ldl(mmt, N, pi)
 
-  var b = new Float64Array(N + handleIds.length)
+  var b = new Float64Array(M)
+  var y = new Float64Array(N)
 
   return function (handlePositions, _out) {
     var out = _out || new Float64Array(3 * N)
@@ -55,7 +57,7 @@ module.exports = function (cells, positions, handleIds) {
       for (var j = 0; j < handlePositions.length; ++j) {
         b[j + N] = handlePositions[j][d]
       }
-      var x = solve(b)
+      var x = solve(augMat.apply(b, y))
       for (var k = 0; k < N; ++k) {
         out[3 * k + d] = x[k]
       }
