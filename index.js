1398e12b546cb77cd6df0dfb32bdacbd80c2e4bd
Eric Arnebäck <arnebackeric@gmail.com>
2017-11-04 22:50:17 +0100

    code simplificaitons.

--- index.js
+++ index.js
@@ -1,8 +1,241 @@
+var mathjs = require('mathjs')
+
+function hypot (x, y, z) {
+  return Math.sqrt(
+    Math.pow(x, 2) +
+      Math.pow(y, 2) +
+      Math.pow(z, 2))
+}
+
+function comparePair (a, b) {
+  return a[0] - b[0] || a[1] - b[1]
+}
+
+var calcLaplacian = function (cells, positions, handlesObj, handlesMap, adj) {
+  var i
+
+  var ha = handlesObj.handles
+
+  var N = ha.length*3
+
+  var buf = new Float64Array(N)
+  var result = []
+
+  for(i = 0; i < N; ++i) {
+
+    for(j = 0; j < N; ++j) {
+      buf[j] = 0
+    }
+
+    buf[i] = 1
+
+    var d = Math.floor(i / ha.length)
+
+    var k = i - d * ha.length
+
+    var w = -1.0 / adj[k].length
+
+    for(var j = 0; j < adj[k].length; ++j) {
+
+      if((d * ha.length +  adj[k][j]) > (d+1)*ha.length ) {
+        console.log("BAD")
+      }
+
+      buf[d * ha.length +  adj[k][j]  ] = w
+    }
+
+    for(var j = 0; j < N; ++j) {
+
+      if(Math.abs(buf[j]) > 1e-7) {
+        result.push([i, j, buf[j]])
+      }
+
+    }
+
+  }
+
+  return result
+}
+
+var calcLaplacianReal = function (cells, positions, delta, handlesObj, invHandlesMap, handlesMap, adj) {
+  var i
+  var ha = handlesObj.handles
+
+  var Ts = []
+
+  for(var i = 0; i < handlesObj.afterHandlesMore; ++i) {
+    // compute transform T_i
+
+    At_coeffs = []
+
+    // set of {i} and N
+    var inset = []
+    inset.push(i)
+    for(var j = 0; j < adj[i].length; ++j) {
+      inset.push(adj[i][j])
+    }
+
+    var At = []
+    for(var row = 0; row < 7; ++row) {
+
+      At[row] = []
+      for(var col = 0; col < inset.length*3; ++col) {
+        At[row][col] = 0
+      }
+    }
+
+    for(var j = 0; j < inset.length; ++j) {
+      var k = inset[j]
+
+      var vk = positions[invHandlesMap[k]]
+      const x = 0
+      const y = 1
+      const z = 2
+
+      At[0][j*3 + 0] =  +vk[x]
+      At[1][j*3 + 0] = 0
+      At[2][j*3 + 0] = +vk[z]
+      At[3][j*3 + 0] = -vk[y]
+      At[4][j*3 + 0] = +1
+      At[5][j*3 + 0] = 0
+      At[6][j*3 + 0] = 0
+
+      At[0][j*3 + 1] = +vk[y]
+      At[1][j*3 + 1] = -vk[z]
+      At[2][j*3 + 1] = 0
+      At[3][j*3 + 1] = +vk[x]
+      At[4][j*3 + 1] = 0
+      At[5][j*3 + 1] = +1
+      At[6][j*3 + 1] = 0
+
+      At[0][j*3 + 2] = +vk[z]
+      At[1][j*3 + 2] = +vk[y]
+      At[2][j*3 + 2] = -vk[x]
+      At[3][j*3 + 2] = 0
+      At[4][j*3 + 2] = 0
+      At[5][j*3 + 2] = 0
+      At[6][j*3 + 2] = 1
+
+    }
+
+    var A = mathjs.transpose(At)
+    var prod = mathjs.multiply(At, A)
+
+    var invprod = mathjs.inv(prod)
+
+    var pseudoinv = mathjs.multiply(invprod, At)
+
+    Ts[i] = pseudoinv
+  }
+
+  var N = handlesObj.handles.length*3
+
+  var buf = new Float64Array(N)
+  var result = []
+
+  for(i = 0; i < N; ++i) {
+
+    for(j = 0; j < N; ++j) {
+      buf[j] = 0
+    }
+
+    // TODO: is this really correct? compare with the of calcLaplacian()
+    buf[i] = 1
+
+    // coordinate component. x=0, y=1, z=1
+    var d = Math.floor(i / handlesObj.handles.length)
+
+    // vertex number.
+    var k = i - d * handlesObj.handles.length
+
+    if(k >= handlesObj.afterHandlesMore) {
+      continue // static vertex, so we specify no information.
+    }
+
+    var w = -1.0 / adj[k].length
+
+    for(var j = 0; j < adj[k].length; ++j) {
+      buf[d * handlesObj.handles.length +  adj[k][j]  ] = w
+    }
+
+    var dx = delta[k + handlesObj.handles.length * 0]
+    var dy = delta[k + handlesObj.handles.length * 1]
+    var dz = delta[k + handlesObj.handles.length * 2]
+
+    var b = []
+    b.push(k) // TODO: wait a minute, should i really be here!?!?!
+    for(var j = 0; j < adj[k].length; ++j) {
+      // TODO: dont we need something like d*positions.length somewhere?
+      b.push(adj[k][j])
+    }
+
+    var T = Ts[k]
+
+    // s is row 0 of T times b.
+    var s = T[0]
+    var h1 = T[1]
+    var h2 = T[2]
+    var h3 = T[3]
+    var tx = T[4]
+    var ty = T[5]
+    var tz = T[6]
+
+    if(d == 0) { // x case.
+      for(var j = 0; j < T[0].length; ++j) {
+        var p = j % 3 // coord component
+        var q = Math.floor(j / 3)
+        var r = b[q] // set member vertex index.
+
+        // buf is xxxxxyyyyyzzz
+        // but s, h3,... are xyzxyzxyz
+        buf[p * handlesObj.handles.length + r] -= dx * (+s[j])
+        buf[p * handlesObj.handles.length + r] -= dy * (-h3[j])
+        buf[p * handlesObj.handles.length + r] -= dz * (+h2[j])
+//     buf[p * handlesObj.handles.length + r] -= 1  * (+tx[j])
+      }
+    } else if(d == 1) { // y case.
+      for(var j = 0; j < T[0].length; ++j) {
+        var p = j % 3 // coord component
+        var q = Math.floor(j / 3)
+        var r = b[q] // set member vertex index.
+
+        buf[p * handlesObj.handles.length + r] -= dx * (+h3[j])
+        buf[p * handlesObj.handles.length + r] -= dy * (+s[j])
+        buf[p * handlesObj.handles.length + r] -= dz * (-h1[j])
+     //   buf[p * handlesObj.handles.length + r] -= 1  * (+ty[j])
+      }
+    } else if(d == 2) { // y case.
+      for(var j = 0; j < T[0].length; ++j) {
+        var p = j % 3 // coord component
+        var q = Math.floor(j / 3)
+        var r = b[q] // set member vertex index.
+
+        buf[p * handlesObj.handles.length + r] -= dx * (-h2[j])
+        buf[p * handlesObj.handles.length + r] -= dy * (+h1[j])
+        buf[p * handlesObj.handles.length + r] -= dz * (+s[j])
+//        buf[p * handlesObj.handles.length + r] -= 1  * (+tz[j])
+      }
+    }
+
+    for(var j = 0; j < N; ++j) {
+
+      if(Math.abs(buf[j]) > 1e-7) {
+        result.push([i, j, buf[j]])
+      }
+
+    }
+
+  }
+
+  result.sort(comparePair)
+
+  return [Ts, result]
+}
+
 var CSRMatrix = require('csr-matrix')
 var cmprecond = require('cuthill-mckee')
 var ldl = require('cholesky-solve').prepare
-var calcLaplacian = require('./src/laplacian').calcLaplacian
-var calcLaplacianReal = require('./src/laplacian').calcLaplacianReal
+
 let li = require("./mylib.js")
 var mathjs = require('mathjs')
 
@@ -14,7 +247,7 @@ let Triplet = li.Triplet
 var csrgemtm = require('./src/csrgemtm')
 
 var qrSolve = require('qr-solve')
-//var qrSolve = require('./lol3.js')
+
 function transpose (positions) {
   var x = new Float64Array(positions.length)
   var y = new Float64Array(positions.length)
@@ -28,13 +261,8 @@ function transpose (positions) {
   return [x, y, z]
 }
 
-function comparePair (a, b) {
-  return a[0] - b[0] || a[1] - b[1]
-}
-
 // sparse matrix coeffs are in the argument.
 function augmentMatrix(coeffsReal, handlesObj, N, M, handlesMap, is2) {
-
   var preprocess = []
 
   if(is2) {
@@ -97,7 +325,6 @@ function augmentMatrix(coeffsReal, handlesObj, N, M, handlesMap, is2) {
 module.exports = function (cells, positions, handlesObj) {
   var ROT_INV = true // rotation invariant algorithm
 
-
   var N = handlesObj.handles.length*3
 
   var numHandles = handlesObj.afterHandles - 0
@@ -105,18 +332,11 @@ module.exports = function (cells, positions, handlesObj) {
 
   var M = N + (numHandles + numStationary)*3
 
-  var trace = new Float64Array(N)
-
   var handlesMap = {}
   var invHandlesMap = {}
   for(var i = 0; i < handlesObj.handles.length; ++i) {
-    // map from 1803,1402 to 0,1
     handlesMap[handlesObj.handles[i]] = i
-
-    // map from 0,1 to 1803,1402
     invHandlesMap[i] = handlesObj.handles[i]
-
-
   }
 
   var ha = handlesObj.handles
@@ -125,7 +345,6 @@ module.exports = function (cells, positions, handlesObj) {
   for(var i = 0; i < ha.length; ++i) {
     adj[handlesMap[ha[i]]] = []
   }
-
   for(var i = 0; i < cells.length; ++i) {
     var c = cells[i]
     for(var j = 0; j < 3; ++j) {
@@ -137,7 +356,7 @@ module.exports = function (cells, positions, handlesObj) {
     }
   }
 
-  var coeffs = calcLaplacian(cells, positions, trace, handlesObj, handlesMap, adj)
+  var coeffs = calcLaplacian(cells, positions, handlesObj, handlesMap, adj)
 
   var lapMat = CSRMatrix.fromList(coeffs, N, N)
 
@@ -166,7 +385,7 @@ module.exports = function (cells, positions, handlesObj) {
   var coeffsReal
   var Ts
   if(ROT_INV) {
-    var a = calcLaplacianReal(cells, positions, trace, delta, handlesObj, invHandlesMap, handlesMap, adj)
+    var a = calcLaplacianReal(cells, positions, delta, handlesObj, invHandlesMap, handlesMap, adj)
     Ts = a[0]
     coeffsReal = a[1]
   } else {
@@ -220,39 +439,12 @@ module.exports = function (cells, positions, handlesObj) {
 
     for (var d = 0; d < 3; ++d) {
       for (var i = 0; i < handlesObj.handles.length; ++i) {
-        //    console.log("before: ", i, positions[invHandlesMap[i]][d])
-        //        console.log("after: ", i, ret.get(i + d*P, 0))
-
         positions[invHandlesMap[i]][d] = ret.get(i + d*(N/3), 0)
       }
     }
 
 
     if(ROT_INV && true) {
-
-     /*
-      var leftside = CSRMatrix.fromList(leftsidemat, M, N)
-      c = 0
-      for (var d = 0; d < 3; ++d) {
-        for (var i = 0; i < handlesObj.handles.length; ++i) {
-          flattened[c++] = positions[handlesObj.handles[i]][d]
-        }
-      }
-      leftside.apply(flattened, othery)
-      console.log("now lets check")
-      for(var p = 0; p < othery.length; ++p) {
-        if(Math.abs(othery[p] - b[p]) < 0.0001) {
-        } else {
-          console.log("NOOO WRONG: ", p)
-        }
-      }
-
-*/
-
-
-
-
-
       c = 0
       for (var d = 0; d < 3; ++d) {
         for (var i = 0; i < handlesObj.handles.length; ++i) {
@@ -264,9 +456,6 @@ module.exports = function (cells, positions, handlesObj) {
 
       var solutionDeltaTrans = []
       for (var i = 0; i < handlesObj.handles.length; ++i) {
-        //    console.log("before: ", i, positions[invHandlesMap[i]][d])
-        //        console.log("after: ", i, ret.get(i + d*P, 0))
-
         solutionDeltaTrans[i] = [
           solutionDelta[i + 0*(N/3)],
           solutionDelta[i + 1*(N/3)],
@@ -312,21 +501,8 @@ module.exports = function (cells, positions, handlesObj) {
         var tz = prod[6]
 
         var rcps = 1.0 / s
-        //        var j = invHandlesMap[i]
 
         var j = i
-
-        if(s > 1.001) {
-//          console.log("PRNT")
-        }
-
-
-//        tempPositions[i] = [rcps*(s * solutionDeltaTrans[j][0] - h3 * solutionDeltaTrans[j][1] + h2 * solutionDeltaTrans[j][2]  + tx ),rcps*(h3 * solutionDeltaTrans[j][0] + s * solutionDeltaTrans[j][1] - h1 * solutionDeltaTrans[j][2] + ty),rcps*(-h2 * solutionDeltaTrans[j][0] + h1 * solutionDeltaTrans[j][1] + s * solutionDeltaTrans[j][2] + tz),]
-
-/*
-        tempPositions[i] = [          rcps*solutionDeltaTrans[i][0],  rcps*solutionDeltaTrans[i][1],rcps*solutionDeltaTrans[i][2]]
-*/
-
 
         var ax = solutionDeltaTrans[i][0]
         var ay = solutionDeltaTrans[i][1]
@@ -337,8 +513,6 @@ module.exports = function (cells, positions, handlesObj) {
         var s = olen / len
 
         tempPositions[i] = [s*solutionDeltaTrans[i][0], s*solutionDeltaTrans[i][1],s*solutionDeltaTrans[i][2]]
-
-
       }
 
       for (var d = 0; d < 3; ++d) {
@@ -356,67 +530,14 @@ module.exports = function (cells, positions, handlesObj) {
 
       var ret = llt2.solvePositiveDefinite(z)
 
-      //  coeffs * positions = y
-
-
-
-
-      var leftside2 = CSRMatrix.fromList(leftsidemat2, M, N)
-      c = 0
       for (var d = 0; d < 3; ++d) {
         for (var i = 0; i < handlesObj.handles.length; ++i) {
-          flattened[c++] = positions[handlesObj.handles[i]][d]
-        }
-      }
-
-      // leftside2.apply(flattened, othery)
-      // console.log("now lets check")
-      // for(var p = 0; p < othery.length; ++p) {
-      //   if(Math.abs(othery[p] - b[p]) < 0.0001) {
-      //   } else {
-      //     console.log("NOOO WRONG: ", p)
-      //   }
-      // }
-
-
-
-
-      for (var d = 0; d < 3; ++d) {
-        for (var i = 0; i < handlesObj.handles.length; ++i) {
-          //    console.log("before: ", i, positions[invHandlesMap[i]][d])
-          //        console.log("after: ", i, ret.get(i + d*P, 0))
-
           positions[invHandlesMap[i]][d] = ret.get(i + d*(N/3), 0)
 
         }
       }
-
-
-
-
-      // we should have that
-      // leftside * positions = b
-      // or is it leftside2?
-
-      // compute T * delta, and put on the right side of equation
-      // T is from Ts.
-      // delta is the solution, in delta coordinates
-
-      //      console.log("prod: ", prod)
-
     }
-
     return out
 
   }
 }
-
-
-
-/*
-  first, see if we actually have a solution.
-
-  compute condition number of matrix.
-
-  then, switch to better mesh.
-*/
